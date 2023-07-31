f4c88e50d076ae12224a084a3ca522c95221109d
Hector Salas Serrano <kenkira666@gmail.com>
2023-07-31 15:29:59 -0500

    API done

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,26 @@ namespace APIsAndJSON
 {
     internal class OpenWeatherMapAPI
     {
+        public static void weatherForCity()
+        {
+            //IConfiguration configuration = new ConfigurationBuilder()
+            //     .SetBasePath(Directory.GetCurrentDirectory())
+            //     .AddJsonFile("appsettings.json")
+            //     .Build();
+
+            var apiKey = JObject.Parse(File.ReadAllText("appsettings.json")).GetValue("ApiKey").ToString();                              
+            //var apiKey = configuration.GetSection("AppSettings")["ApiKey"];                                                                                                                                                                                   
+            var client = new HttpClient();                                                                                                                                                                                                                                      
+            Console.WriteLine("Enter name of city: ");                  
+            var cityName = Console.ReadLine();                      
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+            var weatherResponse = client.GetStringAsync(weatherURL).Result;
+            JObject weatherObject = JObject.Parse(weatherResponse);
+            Console.WriteLine($"It is {weatherObject["main"]["temp"]} degrees fahrenheit in {cityName}");
+
+        }      
+
+
+
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            OpenWeatherMapAPI.weatherForCity();
+
+            HttpClient client = new HttpClient();
+
+            string ronUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            string kanyeUrl = "https://api.kanye.rest/";
+
+            for (int i = 1; i <= 5; i++)
+            {
+
+                var ronResponse = client.GetStringAsync(ronUrl).Result;
+                var ronQuote = JArray.Parse(ronResponse);
+
+                var kanyeResponse = client.GetStringAsync(kanyeUrl).Result;
+                var kanyeQuote = JObject.Parse(kanyeResponse);
+                Console.WriteLine();
+
+                Console.WriteLine($"Ron {ronQuote[0]}");
+                Console.WriteLine($"Kanye {kanyeQuote["quote"]}");
+
+
+            }
         }
     }
 }
